e31b8e0d2c47b2c315ae7c0fcd132863cc02d929
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2026-02-15 22:18:15 +0800

    feat: create article dto

--- src/main/java/com/zhcode/personalbloggingapi/dto/ArticleCreateRequest.java
+++ src/main/java/com/zhcode/personalbloggingapi/dto/ArticleCreateRequest.java
@@ -1,4 +1,29 @@
 package com.zhcode.personalbloggingapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ArticleCreateRequest {
+    @NotBlank
+    @Size(max = 200)
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
