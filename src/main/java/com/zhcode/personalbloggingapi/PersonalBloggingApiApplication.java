3fa802be5f5fbea282bdacba14f7e5b7ad399c8c
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2026-02-15 17:59:30 +0800

    build: init project

---
+++ src/main/java/com/zhcode/personalbloggingapi/PersonalBloggingApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhcode.personalbloggingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingApiApplication.class, args);
+	}
+
+}
